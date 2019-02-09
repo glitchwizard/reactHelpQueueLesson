709707c61b8a906774b13d69bd67d75d9fae4aea
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-08 17:19:52 -0800

    Initial commit, package loaded and working

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+    return ( 
+        <div>
+            <h1>Help Queue</h1>
+            <h3>3A</h3>
+            <h3>Thato and Haley</h3>
+            <p><em>Firebase will not save record</em></p>
+        </div>
+    );
+}
+
+export default App;
