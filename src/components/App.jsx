13a6daabdee5f382eec0d5bc97f04c3cd8770a7c
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-11 08:33:29 -0800

    React test project initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import Header from './Header';
+import TicketList from './TicketList';
 
 function App() {
     return ( 
         <div>
-            <h1>Help Queue</h1>
-            <h3>3A</h3>
-            <h3>Thato and Haley</h3>
-            <p><em>Firebase will not save record</em></p>
+            <Header/>
+            <TicketList/>
         </div>
     );
 }
