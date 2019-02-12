d4c23ed3ccaf2ace4c31445543b31ce6f021ae85
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-11 21:59:53 -0800

    fix PropType validation error in Ticket.jsx

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Ticket({location, names, issue}){
   return (
     <div>
+      <style global jsx>{`
+            div {
+                background-color: red;
+            }
+            div:onclick {
+                background-color: blue;
+            }
+            `}</style>
       <h3>{location} - {names}</h3>
       <p><em>{issue}</em></p>
       <hr/>
     </div>
   );
 }
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
 
 export default Ticket;
