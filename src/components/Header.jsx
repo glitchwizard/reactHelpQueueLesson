74a71a2b859bf3151d677cd680d2607a2daa6ddb
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-11 21:40:50 -0800

    fully implement eslint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 function Header() {
-    return (
-        <h1>Help Queue</h1>
+  return (
+    <h1>Help Queue</h1>
 
-    );
+  );
 }
 
 export default Header;
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 function Ticket({location, names, issue}){
-    return (
-        <div>
-            <h3>{location} - {names}</h3>
-            <p><em>{issue}</em></p>
-            <hr/>
-        </div>
-    );
+  return (
+    <div>
+      <h3>{location} - {names}</h3>
+      <p><em>{issue}</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 export default Ticket;
