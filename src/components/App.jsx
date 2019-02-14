598aa17f7a38fe379a77363bc884a6e2af31b257
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-13 19:58:10 -0800

    add image capability to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import cutestpuppyever from '../assets/images/cutestpuppyever.jpg'
 
 function App() {
   return ( 
     <div>
       <Header/>
       <TicketList/>
+      <img src={cutestpuppyever} style={{maxHeight: '400px'}}/>
     </div>
   );
 }
