5e3d962f48e90adc9226ddec6a10194631a49ef4
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-14 13:23:59 -0800

    --fix linting

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 
 class NewTicketControl extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            formVisibileOnPage: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibileOnPage: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick(){
-        console.log('Hey, you clicked me. Quit that poking around and get something done.')
-    }
+  handleClick(){
+    this.setState({formVisibileOnPage: true});
+    console.log('formVisibleOnPage is currently set to: ' + this.state.formVisibileOnPage);
+        
+  }
 
-    render(){
-        return (
-            <div>
-                <p>This is the NewTicketControl component!</p>
-                <strong onClick={this.handleClick}>Click me to change my state!</strong>
-            </div>
-        );
-    }
+  render(){
+    return (
+      <div>
+        <p>This is the NewTicketControl component!</p>
+        <strong onClick={this.handleClick}>Click me to change my state!</strong>
+      </div>
+    );
+  }
 }
 
 export default NewTicketControl;
