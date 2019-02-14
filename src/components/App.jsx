0ca5b3af67bb7cbaba9ac90b416ebda643d43b2f
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-14 10:15:07 -0800

    update with NewTicketControl

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
-import NewTicketForm from './NewTicketForm';
+import NewTicketControl from './NewTicketControl';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 
@@ -11,10 +11,9 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newTicket' component={NewTicketForm} />
+        <Route path='/newTicket' component={NewTicketControl} />
         <Route component={Error404} />
       </Switch>
-      
     </div>
   );
 }
