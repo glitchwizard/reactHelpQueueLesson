7546074d2ca96ccca7414ee5f356d3e7ab8b8ca9
Charley McGowan and Quinn Miller <charley+quinn@>
2019-02-27 09:57:49 -0800

    move selected ticket information to redux store
    Co-Authored-By: Charley <glitchwizard@gmail.com>
    Co-Authored-By: Quinn <millerquinn66@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { HashRouter } from 'react-router-dom';
 import { createStore } from 'redux';
-import ticketListReducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader'
+import rootReducer from './reducers/index';
 
-const store = createStore(ticketListReducer);
+const store = createStore(rootReducer);
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
