95756429fb9d3f365898d91f4405d6de610fbdb7
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-07 12:54:59 -0800

    no enable Moment()'s to update properly in UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types';
 import constantsImport from './../../src/constants';
 const { constants } = constantsImport;
 import * as actions from './../actions';
+import Moment from 'moment';
 
 class App extends React.Component {
 
@@ -33,7 +34,7 @@ class App extends React.Component {
     const { dispatch } = this.props;
     Object.keys(this.props.masterTicketList).map(ticketId => {
       const ticket = this.props.masterTicketList[ticketId];
-      const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
+      const newFormattedWaitTime = new Moment(ticket.timeOpen).from(new Moment());
       const action = {
         type: constants.UPDATE_TIME,
         id: ticketId,
